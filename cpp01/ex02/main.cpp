e74ec82720d8350a4a19e8f319b47aa9d522ce55
eqramhossain <ekram.fr03@gmail.com>
2026-05-08 01:04:06 +0200

    done cpp01/ex02

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ehossain <ehossain@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/07 21:03:52 by ehossain          #+#    #+#             */
+/*   Updated: 2026/05/08 01:02:45 by ehossain         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int	main(void)
+{
+	std::string stringVAR = "HI THIS IS BRAIN";
+	std::string *stringPTR = &stringVAR;
+	std::string &stringREF = stringVAR;
+
+	std::cout << "The memory address of the  stringVAR: " << &stringVAR << std::endl;
+	std::cout << "The memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
+
+	std::cout << "The value of the stringVAR: " << stringVAR << std::endl;
+	std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
+
+	return (0);
+}
