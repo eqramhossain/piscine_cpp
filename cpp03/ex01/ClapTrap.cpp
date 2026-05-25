22706972d197b29392fc901362fc9e69b5095460
ehossain <ekram.fr03@gmail.com>
2026-05-25 16:43:58 +0200

    yeahh moooore

--- cpp03/ex01/ClapTrap.cpp
+++ cpp03/ex01/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: ehossain <ehossain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/22 19:00:43 by ehossain          #+#    #+#             */
-/*   Updated: 2026/05/23 17:22:22 by ehossain         ###   ########.fr       */
+/*   Updated: 2026/05/25 16:27:17 by ehossain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #include <iostream>
 
 ClapTrap::ClapTrap():
-	_name(""), _hit_point(10), _energy_point(10), _attack_damage(0)
+	_name("Default"), _hit_point(10), _energy_point(10), _attack_damage(0)
 {
 	std::cout << "ClapTrap default constructor called" << std::endl;
 }
@@ -34,7 +34,10 @@ ClapTrap::~ClapTrap()
 ClapTrap::ClapTrap(ClapTrap const &rhs)
 {
 	std::cout << "Copy constructor called, copy from ClapTrap " << rhs._name << std::endl;
-	*this = rhs;
+	this->_name = rhs._name;
+	this->_hit_point = rhs._hit_point;
+	this->_energy_point = rhs._energy_point;
+	this->_attack_damage = rhs._attack_damage;
 }
 
 ClapTrap	&ClapTrap::operator=(ClapTrap const &rhs)
