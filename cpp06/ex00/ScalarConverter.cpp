5d851794c626548ee2ecfc11753a462e41b4b18a
ehossain <ekram.fr03@gmail.com>
2026-07-25 21:11:05 +0200

    at last getting somewhere

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: ehossain <ehossain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/16 17:49:02 by ehossain          #+#    #+#             */
-/*   Updated: 2026/07/25 20:07:48 by ehossain         ###   ########.fr       */
+/*   Updated: 2026/07/25 21:09:26 by ehossain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,17 +48,24 @@ ScalarConverter &ScalarConverter::operator=(ScalarConverter &rhs)
 	return (*this);
 }
 
-bool	isChar(std::string input)
+bool	isChar(std::string str)
 {
-	if (input.size() == 1)
-	{
-		if(std::isprint(input[0]))
-		{
-			return (true);
-		}
-	}
+	if (str.length() == 1 && std::isprint(str[0]) && !std::isdigit(str[0]))
+		return (true);
 	return (false);
 }
+
+// bool	isChar(std::string input)
+// {
+// 	if (input.size() == 1)
+// 	{
+// 		if (std::isprint(static_cast<unsigned char>(input[0])) && !std::isdigit(static_cast<unsigned char>(input[0])))
+// 		{
+// 			return (true);
+// 		}
+// 	}
+// 	return (false);
+// }
 
 bool	isInt(std::string input)
 {
@@ -76,80 +83,101 @@ bool	isInt(std::string input)
 	return (true);
 }
 
-bool	isFloat(std::string input)
+// bool	isFloat(std::string input)
+// {
+// 	int i;
+// 	int found;
+//
+// 	i = 0;
+// 	found = 0;
+// 	if (input.size() < 2)
+// 		return (false);
+// 	if (input[input.size() - 1] == 'f')
+// 	{
+// 		input.erase(input.size() - 1, input.size());
+// 	}
+// 	else 
+// 		return (false);
+//
+// 	if (input[i] == '+' || input[i] == '-')
+// 	{
+// 		i++;
+// 		if (!isdigit(input[i]))
+// 			return (false);
+// 	}
+// 	while(input[i])
+// 	{
+// 		if (std::isdigit(input[i]) == 0)
+// 		{
+// 			if (input[i] == '.')
+// 			{
+// 				found++;
+// 				if (isdigit(input[i + 1]) && isdigit(input[i - 1]))
+// 					return (false);
+// 			}
+// 			else
+// 				return (false);
+// 		}
+// 		i++;
+// 	}
+// 	if (found > 1)
+// 		return (false);
+// 	return (true);
+// }
+
+bool	isFloat(std::string str)
 {
-	int i;
-	int found;
+	int i = 0;
 
-	i = 0;
-	found = 0;
-	if (input.size() < 2)
-		return (false);
-	if (input[input.size() - 1] == 'f')
-	{
-		input.erase(input.size() - 1, input.size());
-	}
-	else 
-		return (false);
-
-	if (input[i] == '+' || input[i] == '-')
-	{
+	if (str[i] == '-' || str[i] == '+')
 		i++;
-		if (!isdigit(input[i]))
-			return (false);
-	}
-	while(input[i])
-	{
-		if (std::isdigit(input[i]) == 0)
-		{
-			if (input[i] == '.')
-			{
-				if (isdigit(input[i + 1]))
-					found++;
-			}
-			else
-				return (false);
-		}
-		i++;
-	}
-	if (found > 1)
+	if (!str[i])
 		return (false);
-	return (true);
+	while (str[i] >= '0' && str[i] <= '9')
+		i++;
+	if (str[i] == '.')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+		i++;
+	if (!str[i] || (str[i] == 'f' && i == static_cast<int>(str.length() - 1)))
+		return (true);
+	return (false);
 }
 
-bool	isDouble(std::string input)
-{
-	int i;
-	int found;
-
-	i = 0;
-	found = 0;
-	if (input.size() < 3)
-		return (false);
-	if (input[i] == '+' || input[i] == '-')
-	{
-		i++;
-		if (!isdigit(input[i]))
-			return (false);
-	}
-	while(input[i])
-	{
-		if (std::isdigit(input[i]) == 0)
-		{
-			if (input[i] == '.')
-			{
-				if (isdigit(input[i + 1]))
-					found++;
-			}
-			else
-				return (false);
-		}
-		i++;
-	}
-	if (found > 1 || found == 0)
-		return (false);
-	return (true);
-}
+// bool	isDouble(std::string input)
+// {
+// 	int i;
+// 	int found;
+//
+// 	i = 0;
+// 	found = 0;
+// 	if (input.size() < 3)
+// 		return (false);
+// 	if (input[i] == '+' || input[i] == '-')
+// 	{
+// 		i++;
+// 		if (!isdigit(input[i]))
+// 			return (false);
+// 	}
+// 	while(input[i])
+// 	{
+// 		if (std::isdigit(input[i]) == 0)
+// 		{
+// 			if (input[i] == '.')
+// 			{
+// 				found++;
+// 				if (isdigit(input[i + 1]) && isdigit(input[i - 1]))
+// 					return (false);
+// 			}
+// 			else
+// 				return (false);
+// 		}
+// 		i++;
+// 	}
+// 	if (found > 1 || found == 0)
+// 		return (false);
+// 	return (true);
+// }
 
 char	ScalarConverter::convertChar(std::string input)
 {
@@ -160,8 +188,9 @@ char	ScalarConverter::convertChar(std::string input)
 		double var = std::atof(input.c_str());
 		if (var > std::numeric_limits<char>::max() || var < 0)
 			return (_char = false, -1);
-		else if (std::isprint(input[0]))
-			return (static_cast<char>(var));
+		unsigned char c = static_cast<unsigned char>(var);
+		if (std::isprint(c) != 0)
+			return(c);
 	}
 	return (_char = false, -1);
 }
@@ -240,8 +269,22 @@ void	ScalarConverter::convert(std::string input)
 		std::cout << "double: " << "nan" << std::endl;
 		return ;
 	}
-
-
+	else if ((!isFloat(input) && !isChar(input)))
+	{
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+		std::cout << "float: impossible" << std::endl;
+		std::cout << "double: impossible" << std::endl;
+		return ;
+	}
+	// if (isChar(input) == false)
+	// 	_char = false;
+	// if (isInt(input) == false)
+	// 	_int = false;
+	// if (isFloat(input) == false)
+	// 	_float = false;
+	// if (isDouble(input) == false)
+	// 	_double = false;
 
 	char c = convertChar(input);
 	int i = convertInt(input);
@@ -255,17 +298,17 @@ void	ScalarConverter::convert(std::string input)
 	else
 		std::cout << "char:\t\t" << c << std::endl;
 	
-	if (i == 0 && _int == false)
+	if (i == -1 && _int == false)
 		std::cout << "int:\t\timpossible" << std::endl;
 	else
 		std::cout << "int:\t\t" << i << std::endl;
 	
-	if (f == 0 && _float == false)
+	if (f == -1 && _float == false)
 		std::cout << "float:\t\timpossible" << std::endl;
 	else
 		std::cout << "float:\t\t" << std::setprecision(1) << std::fixed << f <<  "f" << std::endl;
 
-	if (d == 0 && _double == false)
+	if (d == -1 && _double == false)
 		std::cout << "double:\t\timpossible" << std::endl;
 	else
 		std::cout << "double:\t\t" << std::setprecision(1) << std::fixed << d << std::endl;
