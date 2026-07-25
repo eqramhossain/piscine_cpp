403bd0bcfa9aeefa87e7bb5eb6a9c303e83bd17f
ehossain <ekram.fr03@gmail.com>
2026-07-25 22:48:25 +0200

    it would be more easy i have used strtod() function

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: ehossain <ehossain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/16 17:49:02 by ehossain          #+#    #+#             */
-/*   Updated: 2026/07/25 21:09:26 by ehossain         ###   ########.fr       */
+/*   Updated: 2026/07/25 22:39:19 by ehossain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,11 @@
 # include <iomanip>
 # include <cstdlib>
 
-bool ScalarConverter::_char = true;
-bool ScalarConverter::_int = true;
-bool ScalarConverter::_float = true;
-bool ScalarConverter::_double = true;
+bool ScalarConverter::_char = false;
+bool ScalarConverter::_int = false;
+bool ScalarConverter::_float = false;
+bool ScalarConverter::_double = false;
+
 
 ScalarConverter::ScalarConverter(void)
 {
@@ -55,78 +56,9 @@ bool	isChar(std::string str)
 	return (false);
 }
 
-// bool	isChar(std::string input)
-// {
-// 	if (input.size() == 1)
-// 	{
-// 		if (std::isprint(static_cast<unsigned char>(input[0])) && !std::isdigit(static_cast<unsigned char>(input[0])))
-// 		{
-// 			return (true);
-// 		}
-// 	}
-// 	return (false);
-// }
-
-bool	isInt(std::string input)
+bool	checkInput(std::string str)
 {
-	int i = 0;	
-	if (input[i] == '+' || input[i] == '-')
-		i++;
-	while(input[i])
-	{
-		if (std::isdigit(input[i]) == 0)
-			return (false);
-		i++;
-	}
-	if (input.size() > 11)
-		return (false);
-	return (true);
-}
-
-// bool	isFloat(std::string input)
-// {
-// 	int i;
-// 	int found;
-//
-// 	i = 0;
-// 	found = 0;
-// 	if (input.size() < 2)
-// 		return (false);
-// 	if (input[input.size() - 1] == 'f')
-// 	{
-// 		input.erase(input.size() - 1, input.size());
-// 	}
-// 	else 
-// 		return (false);
-//
-// 	if (input[i] == '+' || input[i] == '-')
-// 	{
-// 		i++;
-// 		if (!isdigit(input[i]))
-// 			return (false);
-// 	}
-// 	while(input[i])
-// 	{
-// 		if (std::isdigit(input[i]) == 0)
-// 		{
-// 			if (input[i] == '.')
-// 			{
-// 				found++;
-// 				if (isdigit(input[i + 1]) && isdigit(input[i - 1]))
-// 					return (false);
-// 			}
-// 			else
-// 				return (false);
-// 		}
-// 		i++;
-// 	}
-// 	if (found > 1)
-// 		return (false);
-// 	return (true);
-// }
-
-bool	isFloat(std::string str)
-{
+	bool	hasDigit = false;
 	int i = 0;
 
 	if (str[i] == '-' || str[i] == '+')
@@ -134,110 +66,24 @@ bool	isFloat(std::string str)
 	if (!str[i])
 		return (false);
 	while (str[i] >= '0' && str[i] <= '9')
+	{
+		hasDigit = true;
 		i++;
+	}
 	if (str[i] == '.')
 		i++;
 	while (str[i] >= '0' && str[i] <= '9')
+	{
+		hasDigit = true;
 		i++;
+	}
 	if (!str[i] || (str[i] == 'f' && i == static_cast<int>(str.length() - 1)))
+	{ 
+		if (!hasDigit)
+			return (false);
 		return (true);
+	}
 	return (false);
-}
-
-// bool	isDouble(std::string input)
-// {
-// 	int i;
-// 	int found;
-//
-// 	i = 0;
-// 	found = 0;
-// 	if (input.size() < 3)
-// 		return (false);
-// 	if (input[i] == '+' || input[i] == '-')
-// 	{
-// 		i++;
-// 		if (!isdigit(input[i]))
-// 			return (false);
-// 	}
-// 	while(input[i])
-// 	{
-// 		if (std::isdigit(input[i]) == 0)
-// 		{
-// 			if (input[i] == '.')
-// 			{
-// 				found++;
-// 				if (isdigit(input[i + 1]) && isdigit(input[i - 1]))
-// 					return (false);
-// 			}
-// 			else
-// 				return (false);
-// 		}
-// 		i++;
-// 	}
-// 	if (found > 1 || found == 0)
-// 		return (false);
-// 	return (true);
-// }
-
-char	ScalarConverter::convertChar(std::string input)
-{
-	if (input.size() == 1 && std::isprint(input[0]))
-		return (static_cast<char>(input[0]));
-	if (input.size() > 1)
-	{
-		double var = std::atof(input.c_str());
-		if (var > std::numeric_limits<char>::max() || var < 0)
-			return (_char = false, -1);
-		unsigned char c = static_cast<unsigned char>(var);
-		if (std::isprint(c) != 0)
-			return(c);
-	}
-	return (_char = false, -1);
-}
-
-int		ScalarConverter::convertInt(std::string input)
-{
-	if (isChar(input))
-		return (static_cast<int>(input[0]));
-	if (input.size() >= 1)
-	{
-		double var = std::atof(input.c_str());
-		if (var > std::numeric_limits<int>::max() || var < -std::numeric_limits<int>::max())
-			return (_int = false, -1);
-		else 
-			return (static_cast<int>(var));
-	}	
-	return (_int = false, -1);
-}
-
-float	ScalarConverter::convertFloat(std::string input)
-{
-	if (isChar(input))
-		return (static_cast<float>(input[0]));
-	if (input.size() >= 1)
-	{
-		double var = std::atof(input.c_str());
-		if (var > std::numeric_limits<float>::max() || var < -std::numeric_limits<float>::max())
-			return (_float = false,-1);
-		else 
-			return (static_cast<float>(var));
-	}
-	return (_float = false, -1);
-}
-
-double	ScalarConverter::convertDouble(std::string input)
-{
-	if (isChar(input))
-		return (static_cast<double>(input[0]));
-	if (input.size() >= 1)
-	{
-		double var = std::atof(input.c_str());
-		if (var > std::numeric_limits<double>::max() || var < -std::numeric_limits<double>::max())
-			return (_double = false,-1);
-		else 
-			return (static_cast<double>(var));
-	}
-	return (_double = false, -1);
 }
 
 void	ScalarConverter::convert(std::string input)
@@ -269,7 +115,7 @@ void	ScalarConverter::convert(std::string input)
 		std::cout << "double: " << "nan" << std::endl;
 		return ;
 	}
-	else if ((!isFloat(input) && !isChar(input)))
+	else if ((!checkInput(input) && !isChar(input)))
 	{
 		std::cout << "char: impossible" << std::endl;
 		std::cout << "int: impossible" << std::endl;
@@ -277,14 +123,6 @@ void	ScalarConverter::convert(std::string input)
 		std::cout << "double: impossible" << std::endl;
 		return ;
 	}
-	// if (isChar(input) == false)
-	// 	_char = false;
-	// if (isInt(input) == false)
-	// 	_int = false;
-	// if (isFloat(input) == false)
-	// 	_float = false;
-	// if (isDouble(input) == false)
-	// 	_double = false;
 
 	char c = convertChar(input);
 	int i = convertInt(input);
@@ -298,18 +136,83 @@ void	ScalarConverter::convert(std::string input)
 	else
 		std::cout << "char:\t\t" << c << std::endl;
 	
-	if (i == -1 && _int == false)
+	if (_int == false)
 		std::cout << "int:\t\timpossible" << std::endl;
 	else
 		std::cout << "int:\t\t" << i << std::endl;
 	
-	if (f == -1 && _float == false)
+	if (_float == false)
 		std::cout << "float:\t\timpossible" << std::endl;
 	else
 		std::cout << "float:\t\t" << std::setprecision(1) << std::fixed << f <<  "f" << std::endl;
 
-	if (d == -1 && _double == false)
+	if (_double == false)
 		std::cout << "double:\t\timpossible" << std::endl;
 	else
 		std::cout << "double:\t\t" << std::setprecision(1) << std::fixed << d << std::endl;
+}
+
+char	ScalarConverter::convertChar(std::string input)
+{
+	_char = true;
+	if (input.size() == 1 && std::isprint(input[0]))
+		return (static_cast<char>(input[0]));
+	if (input.size() >= 1 && checkInput(input))
+	{
+		double var = std::atof(input.c_str());
+		if (var > std::numeric_limits<char>::max() || var < 0)
+			return (_char = false, -1);
+		unsigned char c = static_cast<unsigned char>(var);
+		if (std::isprint(c) != 0)
+			return(c);
+	}
+	return (0);
+}
+
+int		ScalarConverter::convertInt(std::string input)
+{
+	_int = true;
+	if (isChar(input))
+		return (static_cast<int>(input[0]));
+	if (input.size() >= 1 && checkInput(input))
+	{
+		double var = std::atof(input.c_str());
+		if (var > std::numeric_limits<int>::max() || var < std::numeric_limits<int>::min())
+			return (_int = false, 0);
+		else 
+			return (static_cast<int>(var));
+	}	
+	return (_int = false, 0);
+}
+
+float	ScalarConverter::convertFloat(std::string input)
+{
+	_float = true;
+	if (isChar(input))
+		return (static_cast<float>(input[0]));
+	if (input.size() >= 1 && checkInput(input))
+	{
+		double var = std::atof(input.c_str());
+		if (var > std::numeric_limits<float>::max() || var < -std::numeric_limits<float>::max())
+			return (_float = false, 0);
+		else 
+			return (static_cast<float>(var));
+	}
+	return (_float = false, 0);
+}
+
+double	ScalarConverter::convertDouble(std::string input)
+{
+	_double = true;
+	if (isChar(input))
+		return (static_cast<double>(input[0]));
+	if (input.size() >= 1 && checkInput(input))
+	{
+		double var = std::atof(input.c_str());
+		if (var > std::numeric_limits<double>::max() || var < -std::numeric_limits<double>::max())
+			return (_double = false, 0);
+		else 
+			return (static_cast<double>(var));
+	}
+	return (_double = false, 0);
 }
