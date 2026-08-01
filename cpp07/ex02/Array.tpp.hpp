0655ceae714d93d8b5ce00e055046dc445de667e
ehossain <ekram.fr03@gmail.com>
2026-08-01 19:21:17 +0200

    done ex02
    have to review all the exercise template is very complicated

--- cpp07/ex02/Array.tpp.hpp
+++ cpp07/ex02/Array.tpp.hpp
@@ -6,98 +6,123 @@
 /*   By: ehossain <ehossain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/31 20:06:30 by ehossain          #+#    #+#             */
-/*   Updated: 2026/07/31 21:13:14 by ehossain         ###   ########.fr       */
+/*   Updated: 2026/08/01 19:20:45 by ehossain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARRAY_TPP_HPP
 #define ARRAY_TPP_HPP
 
-#include "Array.hpp"
 #include <iostream>
+#include <ostream>
+
+template <typename T>
+class Array 
+{
+	private:
+		T *_array;
+		unsigned int _size_array;
+
+	public:
+		Array();
+		Array(const unsigned int n);
+		~Array();
+
+		Array(const Array &rhs);
+		Array &operator=(const Array &rhs);
+
+		T &operator[](unsigned int index);
+		const T &operator[](unsigned int index) const;
+		
+		unsigned int size(void) const;
+		
+		class OutOfBound : public std::exception 
+		{
+			virtual const char	*what() const throw();
+		};
+};
 
 template <typename T> 
 Array<T>::Array()
-	: _array(new int(0)), 
+	: _array(NULL), 
 	_size_array(0)
 {
 	std::cout << "Defalut Constructor Called" << std::endl;
-	std::cout << *_array << std::endl;
-	std::cout << _size_array << std::endl;
 }
 
 template <typename T>
 Array<T>::Array(const unsigned int n)
-	: _array(new int(n)),
+	: _array(new T[n]()),
 	_size_array(n)
 {
 	std::cout << "Parameteraized Constructor Called" << std::endl;
-	// std::cout << _array << std::endl;
-	// for (unsigned int i = 0; i < n; i++)
-	// {
-	// 	_array[i] = i;
-	// 	std::cout << _array[i] << std::endl;
-	// }
-	std::cout << *_array << std::endl;
-	std::cout << _size_array << std::endl;
 }
 
 template <typename T>
 Array<T>::~Array()
 {
 	std::cout << "Destructor Called" << std::endl;
-	delete _array;
+	delete [] _array;
 }
 
 template <typename T>
 Array<T>::Array(const Array<T> &rhs)
-	: _array(new int(rhs._size_array)),
+	: _array(new T[rhs._size_array]),
 	_size_array(rhs._size_array)
 {
 	std::cout << "Copy Constructor Called" << std::endl;
-	std::cout << *_array << std::endl;
-	std::cout << _size_array << std::endl;
-
+	for (unsigned int i = 0; i < rhs._size_array; i++)
+	{
+		_array[i] = rhs._array[i];
+	}
 }
 
 template <typename T>
 Array<T> &Array<T>::operator=(const Array<T> &rhs)
 {
 	std::cout << "Copy Assignment Operator Called" << std::endl;
-	if (this != *rhs)
+	if (this != &rhs)
 	{
 		delete [] _array;
-		_array = new int(rhs._size_array);
+		_array = new T[rhs._size_array];
 		_size_array = rhs._size_array;
+		for (unsigned int i = 0; i < rhs._size_array; i++)
+		{
+			_array[i] = rhs._array[i];
+		}
 	}
-	std::cout << *_array << std::endl;
-	std::cout << _size_array << std::endl;
 	return (*this);
 }
 
 template <typename T>
+T &Array<T>::operator[](unsigned int index)
+{
+	if (index < _size_array)
+		return (_array[index]);
+	else 
+		throw Array<T>::OutOfBound();
+}
+
+template <typename T>
+const T &Array<T>::operator[](unsigned int index) const
+{
+	if (index > _size_array)
+		throw Array<T>::OutOfBound();
+	else 
+		return (_array[index]);
+}
+
+
+template <typename T>
 unsigned int Array<T>::size(void) const
 {
-	std::cout << "size= " << this->_size_array << std::endl;
 	return (this->_size_array);
 }
 
-// template <typename T>
-// const char *Array<T>::OutOfBound::what() const throw()
-// {
-// 	return ("memory out of range");
-// }
-
-// template <typename T>
-// std::ostream	&operator<<(std::ostream &out, const Array<T> &rhs)
-// {
-// 	for (unsigned int i = 0; i < rhs.size() ; i++)
-// 	{
-// 		std::cout << "_array[" << i << "]" << " = " << rhs.get_value([i]) << std::endl;
-// 	}
-//
-// 	return (out);
-// }
-
+template <typename T>
+const char *Array<T>::OutOfBound::what() const throw()
+{
+	return ("index is out of bound");
+}
 
 #endif
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -6,21 +6,152 @@
 /*   By: ehossain <ehossain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/30 11:50:34 by ehossain          #+#    #+#             */
-/*   Updated: 2026/07/31 21:07:58 by ehossain         ###   ########.fr       */
+/*   Updated: 2026/08/01 19:19:20 by ehossain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include "Array.hpp"
 #include "Array.tpp.hpp"
 
-int main(void)
-{
-	Array<unsigned int> a;
-	Array<unsigned int> b(50);
-	Array<unsigned int> c(b);
-	Array<unsigned int> d = a;
+// int main(void)
+// {
+// 	Array<unsigned int> a;
+// 	Array<unsigned int> b(50);
+// 	// Array<unsigned int> c(b);
+// 	// Array<unsigned int> d = a;
+//
+// 	return (0);
+// }
 
-	d.size();
-	return (0);
+int main()
+{
+    std::cout << "===== DEFAULT CONSTRUCTOR =====" << std::endl;
+
+    Array<int> empty;
+
+    std::cout << "Size: " << empty.size() << std::endl;
+
+
+    std::cout << "\n===== PARAMETERIZED CONSTRUCTOR =====" << std::endl;
+
+    Array<int> numbers(5);
+
+    std::cout << "Size: " << numbers.size() << std::endl;
+
+    for (unsigned int i = 0; i < numbers.size(); i++)
+        std::cout << numbers[i] << " ";
+
+    std::cout << std::endl;
+
+
+    std::cout << "\n===== WRITE VALUES =====" << std::endl;
+
+    for (unsigned int i = 0; i < numbers.size(); i++)
+        numbers[i] = i * 10;
+
+    for (unsigned int i = 0; i < numbers.size(); i++)
+        std::cout << numbers[i] << " ";
+
+    std::cout << std::endl;
+
+
+    std::cout << "\n===== OUT OF BOUNDS EXCEPTION =====" << std::endl;
+
+    try
+    {
+        std::cout << numbers[5] << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << "Exception: " << e.what() << std::endl;
+    }
+
+
+    std::cout << "\n===== COPY CONSTRUCTOR =====" << std::endl;
+
+    Array<int> copy(numbers);
+
+    std::cout << "Original: ";
+    for (unsigned int i = 0; i < numbers.size(); i++)
+        std::cout << numbers[i] << " ";
+
+    std::cout << std::endl;
+
+    std::cout << "Copy: ";
+    for (unsigned int i = 0; i < copy.size(); i++)
+        std::cout << copy[i] << " ";
+
+    std::cout << std::endl;
+
+
+    std::cout << "\n===== DEEP COPY TEST =====" << std::endl;
+
+    copy[0] = 999;
+
+    std::cout << "Original first element: "
+              << numbers[0] << std::endl;
+
+    std::cout << "Copy first element: "
+              << copy[0] << std::endl;
+
+
+    std::cout << "\n===== COPY ASSIGNMENT =====" << std::endl;
+
+    Array<int> assigned;
+
+    assigned = numbers;
+
+    std::cout << "Assigned: ";
+
+    for (unsigned int i = 0; i < assigned.size(); i++)
+        std::cout << assigned[i] << " ";
+
+    std::cout << std::endl;
+
+
+    std::cout << "\n===== ASSIGNMENT DEEP COPY TEST =====" << std::endl;
+
+    assigned[1] = 777;
+
+    std::cout << "Original second element: "
+              << numbers[1] << std::endl;
+
+    std::cout << "Assigned second element: "
+              << assigned[1] << std::endl;
+
+
+    std::cout << "\n===== SELF ASSIGNMENT =====" << std::endl;
+
+    numbers = numbers;
+
+    for (unsigned int i = 0; i < numbers.size(); i++)
+        std::cout << numbers[i] << " ";
+
+    std::cout << std::endl;
+
+
+    std::cout << "\n===== CONST ACCESS =====" << std::endl;
+
+    const Array<int> constArray(numbers);
+
+    std::cout << "Const element: "
+              << constArray[0]
+              << std::endl;
+
+
+    std::cout << "\n===== STRING TEMPLATE TEST =====" << std::endl;
+
+    Array<std::string> words(3);
+
+    words[0] = "hello";
+    words[1] = "world";
+    words[2] = "cpp07";
+
+    for (unsigned int i = 0; i < words.size(); i++)
+        std::cout << words[i] << std::endl;
+
+
+    std::cout << "\n===== PROGRAM END =====" << std::endl;
+
+    return (0);
 }
