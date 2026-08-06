bd174f3af0a6bfb45e55b64a7bc16504788575c0
ehossain <ekram.fr03@gmail.com>
2026-08-06 20:33:36 +0200

    non const to const var

--- cpp07/ex01/iter.h
+++ cpp07/ex01/iter.h
@@ -6,7 +6,7 @@
 /*   By: ehossain <ehossain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/29 12:50:30 by ehossain          #+#    #+#             */
-/*   Updated: 2026/07/30 11:41:42 by ehossain         ###   ########.fr       */
+/*   Updated: 2026/08/06 20:31:04 by ehossain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include <iostream>
 
 template <typename X, typename Y>
-void	iter(X *ptr,size_t size, Y const function)
+void	iter(X *ptr, const size_t size, Y const function)
 {	
 	size_t i = 0;
 	while(i < size)
